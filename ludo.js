b3133c863e7c8a585defad52da5c0e3d6c03edf1
Danny-Wits <114674838+Danny-Wits@users.noreply.github.com>
2023-09-01 16:12:57 +0530

    v

--- ludo.js
+++ ludo.js
@@ -1,4 +1,5 @@
 const board= document.getElementById("map");
+console.log("LOADED");
 for (let i = 0; i < 15; i++) {
     for (let j = 0; j < 15; j++) {
         let span = document.createElement("span");
